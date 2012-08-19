165eadcad5d7a64b6e5959b8d4bfae5dcd848855
Luke Stevenson <luke@lucanos.com>
2012-08-20 03:31:42 +1000

    Re-Engineered PageData script

--- pageData.php
+++ pageData.php
@@ -1,110 +1,121 @@
 <?php
 
-/**
- * Keep in mind:
- * The code in this example is hard-coded, so previous/next and sorting will not work in the demo.
- * It was left out intentionally since this was designed to pull and order from another data source (like a database)
- *
- * The sample below is what you should build your code to look like when you incorporate your database or other storage system.
- * You'll also need to have the json_encode() function available to you. Otherwise you'll need another library like the Zend Framework.
- *
- **/
+//###
+//#
+//#  Compass Datagrid - Server-side code
+//#
+//###
 
-// Set json content-type header
-header("Content-Type: text/x-json");
 
-// Must let the datagrid know current PAGINATION info
-// Page: The Current Page
-// Pages: Total number of pages found
-// Found: Total number of results found
-// Displaying start: The First result we are _currently_ showing
-// Displaying End: The last result we are _currently_ showing
-$pager = array(array(
-	'page'				=>	1,
-	'pages'				=> 	1,
-	'found' 			=>	5,
-	'displayingStart'	=> 	1,
-	'displayingEnd'		=> 	5
-));
+ // Page Specific Variables
 
-// Each array represents a column
-// Each column must have:
-	// ID	an unique identifier for that column
-	// Display		the text to display in that column
-	// Width		the width of that column (pixels or percentage)
-	// Sort			optional. either sort-asc or sort-desc if you are currently sorting by that column
-$headings = array(
-	array(
-		'id'			=>		'one',
-		'display'		=>		'One',
-		'width'			=>		'25%',
-		'sort'			=>		''
-	),
-	array(
-		'id'			=>		'two',
-		'display'		=>		'Two',
-		'width'			=>		'25%',
-		'sort'			=>		'sort-asc'
-	),
-	array(
-		'id'			=>		'three',
-		'display'		=>		'Three',
-		'width'			=>		'25%',
-		'sort'			=>		''
-	),
-	array(
-		'id'			=>		'four',
-		'display'		=>		'Four',
-		'width'			=>		'25%',
-		'sort'			=>		''
-	)
-);
+ # Debug Switch
+  define( 'DEBUG' , false );
+ # Default Options
+  $defaults = array(
+    'page'      => 1 ,
+    'show'      => 10 ,
+    'showField' => 'id' ,
+    'showOrder' => 'asc'
+  );
+ # Translate Table IDs to Titles
+  $idToTitle = array(
+    'id'    => 'Record #' ,
+    'two'   => 'Col #2' ,
+    'three' => 'Col #3' ,
+    'four'  => 'Col #4'
+  );
+ # Column Widths (Optional)
+  $idToWidth = array(
+    'two' => '50%'
+  );
 
-// Each array items represents a new row
-// The array keys correspond to the id in the headings array above
-$rows = array(
-	array(
-		'one' 	=> 'A1', 
-		'two'	=>	'A2', 
-		'three'	=>	'A3', 
-		'four'	=> 	'A4',
-	),
-	array(
-		'one' 	=> 'B1', 
-		'two'	=>	'B2', 
-		'three'	=>	'B3', 
-		'four'	=> 	'B4',
-	),
-	array(
-		'one' 	=> 'C1', 
-		'two'	=>	'C2', 
-		'three'	=>	'C3', 
-		'four'	=> 	'C4',
-	),
-	array(
-		'one' 	=> 'D1', 
-		'two'	=>	'D2', 
-		'three'	=>	'D3', 
-		'four'	=> 	'D4',
-	),
-	array(
-		'one' 	=> 'E1', 
-		'two'	=>	'E2', 
-		'three'	=>	'E3', 
-		'four'	=> 	'E4',
-	)
-);
+ # Check for GET Parameters and Validate
+  $request = $defaults;
+  if( count( $_GET ) ){
+    if( isset( $_GET['page'] ) )
+      $request['page'] = (int) $_GET['page'];
+    if( isset( $_GET['show'] ) )
+      $request['show'] = (int) $_GET['show'];
+    if( isset( $_GET['showField'] ) && in_array( $_GET['showField'] , array_keys( $idToTitle ) ) )
+      $request['showField'] = (int) $_GET['showField'];
+    if( isset( $_GET['showOrder'] ) && in_array( $_GET['showOrder'] , array( 'asc' , 'desc' ) ) )
+      $request['showOrder'] = $_GET['showOrder'];
+  }
 
-// Put it all in an array
-$json = array(
-	'pager' => $pager,
-	'headings' => $headings,
-	'rows' => $rows
-);
 
-// Encode it, then send it to the browser!
-echo json_encode($json);
+ // Page Specific Functions
 
-exit;
+ # Processes the three pieces of data into JSON
+  function sendAsJSON( $pageData=null , $headingData=null , $contentData=null ){
+   # Check Data
+    if( is_null( $pageData ) || is_null( $headingData ) || is_null( $contentData ) )
+      die( 'Error: Required data not provided.' );
+   # Compile Array
+    $out = array();
+    if( is_array( $pageData ) ){
+      if( is_array( $pageData[0] ) )
+        $out['pager'] = $pageData;
+      else
+        $out['pager'] = array( $pageData );
+    }
+    $out['headings'] = $headingData;
+    $out['rows'] = $contentData;
+   # Send JSON
+    if( DEBUG ){
+      echo '<pre>';
+      var_dump( $out );
+      die();
+    }
+    header( 'Content-Type: text/x-json' );
+    die( json_encode( $out ) );
+  }
 
-?>
+
+ // Query Database/Source and Create Content
+
+ # The Output Data
+  $data = array();
+  $totalDataCount = 0;
+
+ # DEMONSTRATION DATA - START
+  $totalDataCount = 95;
+  for( $i = ( ( $request['page']-1 )*$request['show'] )+1 , $c = min( $request['show'] * $request['page'] , $totalDataCount ) ; $i<=$c ; $i++ ){
+    $data[] = array(
+      'id'    => $i ,
+      'two'   => 'B'.$i ,
+      'three' => 'C'.$i ,
+      'four'  => 'D'.$i
+    );
+  }
+ # DEMONSTRATION DATA - END
+
+
+ // Prepare the Output
+ 
+ # Pager Data (Automated)
+  $pager = array(array(
+    'page'            => $request['page'] ,
+    'pages'           => ceil( $totalDataCount / $request['show'] ) ,
+    'found'           => $totalDataCount ,
+    'displayingStart' => ( ( $request['page'] - 1 )*$request['show'] ) + 1 ,
+    'displayingEnd'   => ( ( $request['page'] - 1 )*$request['show'] ) + count( $data )
+  ));
+
+ # Headings (Automated)
+  $headings = array();
+  foreach( $idToTitle as $id => $title ){
+    $r = array(
+      'id' => $id ,
+      'display' => $title
+    );
+    if( isset( $idToWidth[$id] ) )
+      $r['width'] = $idToWidth[$id];
+    if( $request['showField']==$id )
+      $r['sort'] = 'sort-'.$request['showOrder'];
+    $headings[] = $r;
+  }
+
+
+ // Transmit the JSON Output
+  sendAsJSON( $pager , $headings , $data );
